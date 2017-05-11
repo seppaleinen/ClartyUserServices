98f980f9ea2b76de916f7a81f64d086eaeaef192
axeljansson <janssonaxel1@gmail.com>
2017-05-11 15:53:26 +0200

    Added authentication for user-services with JWT.

--- user-services/src/main/java/se/claremont/backend/user/api/UserController.java
+++ user-services/src/main/java/se/claremont/backend/user/api/UserController.java
@@ -3,8 +3,9 @@ package se.claremont.backend.user.api;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 class UserController {
@@ -13,5 +14,17 @@ class UserController {
     ResponseEntity<String> ping() {
         return ResponseEntity.ok("Hello");
     }
+    
+    @RequestMapping("/")
+    String hello() {
+        return "hello world";
+    }
 
+    /* Maps to all HTTP actions by default (GET,POST,..)*/
+    @RequestMapping("/users")
+    public @ResponseBody String getUsers() {
+      return "{\"users\":[{\"firstname\":\"Richard\", \"lastname\":\"Feynman\"}," +
+             "{\"firstname\":\"Marie\",\"lastname\":\"Curie\"}]}";
+    }
+    
 }
