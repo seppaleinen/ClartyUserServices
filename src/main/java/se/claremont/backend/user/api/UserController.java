7c020ca52fcfe54083c497a84c84a003c60537e7
David Eriksson <davideriksson@swedenmail.com>
2017-06-12 18:54:40 +0200

    Removed unneccesary @ResponseBody from return in UserController

--- src/main/java/se/claremont/backend/user/api/UserController.java
+++ src/main/java/se/claremont/backend/user/api/UserController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import se.claremont.backend.user.repository.UserRepository;
 import se.claremont.backend.user.repository.entities.User;
@@ -18,18 +17,18 @@ class UserController {
     private UserRepository userRepository;
 
     @GetMapping(path = "/ping", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @ResponseBody String ping() {
+    String ping() {
         return "Hello";
     }
     
     @RequestMapping("/")
-    @ResponseBody String hello() {
+    String hello() {
         return "hello world";
     }
 
     /* Maps to all HTTP actions by default (GET,POST,..)*/
     @RequestMapping(value = "/users")
-    @ResponseBody List<String> getUsers() {
+    List<String> getUsers() {
         return userRepository.findAll()
                         .stream()
                         .map(User::getUsername)
